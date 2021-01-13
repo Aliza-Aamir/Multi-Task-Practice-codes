1050f5efecd8baaf6995b42c285ea2f4b2e04a3b
Aliza Aamir <73904031+Aliza-Aamir@users.noreply.github.com>
2021-01-13 14:18:25 +0500

    Add files via upload

---
+++ First 5 square numbers.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim userinput, X, N As Integer
+        Dim count As Integer
+
+        userinput = 0
+        count = 0
+
+        Console.WriteLine("This algorithm outputs the first 5 square numbers which comes after whatever number you enter. i.e input = 3 will give you a square of 3, 4, 5, 6, 7 and 8.")
+        Console.WriteLine("Your input: ")
+        userinput = Console.ReadLine()
+        Console.WriteLine("*******")
+
+        X = userinput * userinput
+        Console.WriteLine(X)
+
+        For count = 0 To 4
+            userinput = userinput + 1
+            X = userinput * userinput
+            Console.WriteLine(X)
+        Next count
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
